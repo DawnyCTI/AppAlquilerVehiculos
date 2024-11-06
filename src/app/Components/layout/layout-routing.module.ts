f688fa8c3ba61c9decab8501783d2973da46cc42
DawnyCTI <202221014@itla.edu.do>
2024-11-06 19:07:53 -0400

    feat: Agregar archivos de estilo y plantillas para las páginas de venta, reporte, usuario, vehículo, dash-board y historial-venta

--- src/app/Components/layout/layout-routing.module.ts
+++ src/app/Components/layout/layout-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout.component';
+import { DashBoardComponent } from './Pages/dash-board/dash-board.component';
+import { UsuarioComponent } from './Pages/usuario/usuario.component';
+import { VehiculoComponent } from './Pages/vehiculo/vehiculo.component';
+import { VentaComponent } from './Pages/venta/venta.component';
+import { HistorialVentaComponent } from './Pages/historial-venta/historial-venta.component';
+import { ReporteComponent } from './Pages/reporte/reporte.component';
+
+const routes: Routes = [{
+  path: '',
+  component: LayoutComponent,
+  children: [
+    { path: 'dashboard', component:DashBoardComponent },
+    { path: 'usuario', component:UsuarioComponent },
+    { path: 'vehiculo', component:VehiculoComponent },
+    { path: 'venta', component:VentaComponent },
+    { path: 'historial-venta', component:HistorialVentaComponent },
+    { path: 'reporte', component:ReporteComponent }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// Removed incorrect import of AppRoutingModule
+import { LayoutComponent } from './Components/layout/layout.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import path from 'path';
 
-
+const routes: Routes = [
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'pages', loadChildren: () => import('./Components/layout/layout.module').then(m => m.LayoutModule) },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
